1c9d6a9608b6a633f113bacfae275d7dc28d0b50
Essam-2 <esamanwar2@gmail.com>
2022-04-06 11:47:51 +0300

    add essam work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
-  content: [],
+  // content: ["./index.html","./sign-up.html","./login.html"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'body': '#052540',
+        'selected-text': '#FFC95A',
+        'theme': '#FFB215',
+        'nav' : '#404053',
+        'secondary': '#9191a4',
+        'badge' : '#3f3f51',
+        'input-border': '#565666',
+        'input': '#2a2a35'
+      },
+      fontFamily:{
+        'poppins':["'Poppins' , 'sans-serif'"]
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/images/Background-Image-5.png')"
+      }
+    },
   },
   plugins: [],
 }
